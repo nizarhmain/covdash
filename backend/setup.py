42ca540873f930787d9b7320a1d0ea08d8572583
nizarhmain <hmain.nizar@gmail.com>
2021-01-24 18:54:19 +0100

    fixed the pytest and imports

--- backend/setup.py
+++ backend/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="covdash", packages=find_packages())
+setup(name="backend", packages=find_packages())
--- backend/tests/test_one.py
+++ backend/tests/test_one.py
@@ -1,13 +1,23 @@
-from backend.crawling.parser import Parser 
+from crawling.parser import Parser
 
 import pytest
+
+import os
 
 
 def func(x):
     return x + 1
 
 
+def test_good_csv():
+    Parser("tests/test_data").parse("dpc-covid19-ita-regioni-20200224.csv")
+    # check if the geojson was generated
+    assert(os.path.exists("tests/test_data/geojson/dpc-covid19-ita-regioni-20200224.csv.json")) == True
+    # delete the file
+    os.remove("tests/test_data/geojson/dpc-covid19-ita-regioni-20200224.csv.json")
+
+
+
 def test_bad_csv():
     with pytest.raises(Exception) as excinfo:
         Parser("tests/test_data").parse("bad_csv.csv")
-
