ca6eac9baf4cb97e7d7f97a83be86726e36e3368
nizarhmain <hmain.nizar@gmail.com>
2021-01-25 19:45:16 +0100

    added requiments

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,32 @@ from flask_cors import CORS
 
 import os
 
+import time
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from crawling import crawling_script
+
 
 app = Flask(__name__)
 CORS(app)
+
+
+
+
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+    crawling_script.crawl()
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", hours=6)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
 
 
 @app.route('/')
@@ -35,3 +58,5 @@ def get_geojson():
         return send_file(file)
     except FileNotFoundError:
         return abort(404)
+
+
--- backend/crawling/crawling_script.py
+++ backend/crawling/crawling_script.py
@@ -5,28 +5,28 @@ from crawling.github_scraper import scrape
 from progress.bar import Bar
 import os 
 
+def crawl():
+   # check if the data folder exists
+   # create them if they dont
 
-# check if the data folder exists
-# create them if they dont
-
-if (os.path.exists('data/csv') == False or os.path.exists('data/geojson') == False):
-   os.mkdir('data')
-   os.mkdir('data/csv')
-   os.mkdir('data/geojson')
+   if (os.path.exists('data/csv') == False or os.path.exists('data/geojson') == False):
+      os.mkdir('data')
+      os.mkdir('data/csv')
+      os.mkdir('data/geojson')
 
 
-# first download from git
-scrape()
+   # first download from git
+   scrape()
 
-# parse all the csvs from git
+   # parse all the csvs from git
 
-p = Parser("data")
+   p = Parser("data")
 
-csvs = os.listdir("data/csv")
+   csvs = os.listdir("data/csv")
 
-bar = Bar('Parsing', max=len(csvs))
-for csv in csvs:
-   Parser("data").parse(csv)
-   bar.next()
+   bar = Bar('Parsing', max=len(csvs))
+   for csv in csvs:
+      Parser("data").parse(csv)
+      bar.next()
 
-bar.finish()
+   bar.finish()
